40df7a7cd58301d2356a642ed8e91fdc68633f28
Alejandro Queiruga <afqueiruga@lbl.gov>
2016-07-20 14:19:19 -0700

    Made a class.

--- mydb.py
+++ mydb.py
@@ -1,14 +1,57 @@
 import sqlite3
 
+class SimDataDB():
+    def __init__(self,dbase):
+        self.dbase = dbase
+        self.conn = sqlite3.connect(dbase)
+        self.dbs = {}
+        
+    def Add_Table(self, table, args):
+        c = self.conn.cursor()
+        columns = [ '{0} {1}'.format(k[0],k[1]) for k in args ]
+        try:
+            c.execute('CREATE TABLE if not exists {0} ({1})'.format(table, ', '.join(columns)) )
+        except:
+            pass
+        # save the argument list to this table
+        self.dbs[table] = args
+        self.conn.commit()
+        
+    def Decorate(self,table):
+        def wrap(f):
+            def wrapper(*args):
+                c = self.conn.cursor()
+                # check if arguments exist already
+                
+                # call the simulation
+                ret = f(*args)
+                # push args into dbase
+                c.execute("INSERT INTO {0} VALUES ({1})".format(table, ", ".join([ str(a) for a in args]) ) )
+                # commit
+                self.conn.commit()
+                # behave like the original
+                return ret
+            return wrapper
+        return wrap
+    
+    def Grab_All(self, table):
+        c = self.conn.cursor()
+        c.execute("SELECT * FROM {0}".format(table))
+        rows = c.fetchall()
+        return rows
+        
+    def __del__(self):
+        self.conn.commit()
+        self.conn.close()
 
+
+    
 def setupDB(dbase, table, args):
     conn = sqlite3.connect(dbase)
     c = conn.cursor()
     columns = [ '{0} {1}'.format(k[0],k[1]) for k in args ]
-    try:
-        c.execute('CREATE TABLE {0} ({1})'.format(table, ', '.join(columns)) )
-    except:
-        pass
+    c.execute('CREATE TABLE if not exists {0} ({1})'.format(table, ', '.join(columns)) )
+
     conn.commit()
     conn.close()
     
@@ -18,7 +61,7 @@ def scripter(dbase, table):
             # check if args already in dbase
             conn = sqlite3.connect(dbase)
             c = conn.cursor()
- s           # c.execute("SELECT EXISTS(SELECT 1 FROM {0} WHERE {1} LIMIT 1)".fromat(table),)
+            # c.execute("SELECT EXISTS(SELECT 1 FROM {0} WHERE {1} LIMIT 1)".fromat(table),)
             # call the simulation
             ret = f(*args)
             # push args into dbase
@@ -36,11 +79,7 @@ def scripter(dbase, table):
 
 def grab_all(dbase,table):
     con = sqlite3.connect(dbase)
-    with con:
-        c = con.cursor()
-        c.execute("SELECT * FROM {0}".format(table))
-        rows = c.fetchall()
-        return rows
+
 def runner(S, args):
     pass
 
--- testA.py
+++ testA.py
@@ -1,12 +1,17 @@
-import mydb
+from mydb import *
 
 
-mydb.setupDB("testA.db", "results1",( ("x","FLOAT"), ("y","FLOAT") ))
+sdb = SimDataDB("testA.db")
+sdb.Add_Table("results1",( ("x","FLOAT"), ("y","FLOAT") ))
 
-@mydb.scripter("testA.db", "results1")
+# mydb.setupDB("testA.db", "results1",( ("x","FLOAT"), ("y","FLOAT") ))
+
+# @mydb.scripter("testA.db", "results1")
+@sdb.Decorate("results1")
 def fake_sim(x,y):
     return x+2.0*y
 
 fake_sim(1.0,1.5)
 
-runs = mydb.grab_all("testA.db", "results1")
+rubs = sdb.Grab_All("results1")
+# runs = sdb.grab_all("testA.db", "results1")
