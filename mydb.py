230cde46bfcaf882ccd74e23d8ff6e3a87079cf9
Alejandro Francisco Queiruga <afq@berkeley.edu>
2016-07-20 15:22:04 -0700

    Added return signature.

--- mydb.py
+++ mydb.py
@@ -1,18 +1,47 @@
 import sqlite3
+import numpy as np
+import io
+
+##
+# BEGIN CITATION:
+# http://stackoverflow.com/questions/18621513/python-insert-numpy-array-into-sqlite3-database
+#
+def adapt_array(arr):
+    """
+    http://stackoverflow.com/a/31312102/190597 (SoulNibbler)
+    """
+    out = io.BytesIO()
+    np.save(out, arr)
+    out.seek(0)
+    return sqlite3.Binary(out.read())
+def convert_array(text):
+    out = io.BytesIO(text)
+    out.seek(0)
+    return np.load(out)
+# Converts np.array to TEXT when inserting
+sqlite3.register_adapter(np.ndarray, adapt_array)
+# Converts TEXT to np.array when selecting
+sqlite3.register_converter("array", convert_array)
+#
+# end citation
+##
 
 class SimDataDB():
     def __init__(self,dbase):
         self.dbase = dbase
-        self.conn = sqlite3.connect(dbase)
-        self.dbs = {}
+        self.conn = sqlite3.connect(dbase, detect_types=sqlite3.PARSE_DECLTYPES)
+        self.callsigs = {}
+        self.retsigs = {}
         
-    def Add_Table(self, table, callsig):
+    def Add_Table(self, table, callsig, retsig):
         c = self.conn.cursor()
-        columns = [ '{0} {1}'.format(k[0],k[1]) for k in callsig ]
+        columns = [ '{0} {1}'.format(k[0],k[1]) for k in callsig + retsig ]
+        
         c.execute('CREATE TABLE IF NOT EXISTS {0} ({1})'.format(table, ', '.join(columns)) )
 
         # save the argument list to this table
-        self.dbs[table] = callsig
+        self.callsigs[table] = callsig
+        self.retsigs[table] = retsig
         self.conn.commit()
         
     def Decorate(self,table):
@@ -21,7 +50,7 @@ class SimDataDB():
                 c = self.conn.cursor()
                 # check if arguments exist already
                 argcheck = " AND ".join([ "{0}={1}".format(argname[0],val)
-                             for argname,val in zip(self.dbs[table], args) ])
+                             for argname,val in zip(self.callsigs[table], args) ])
                 c.execute("SELECT EXISTS(SELECT 1 FROM {0} WHERE {1} LIMIT 1)".format(table, argcheck) )
                 
                 if c.fetchall()[0][0]==1:
@@ -29,7 +58,8 @@ class SimDataDB():
                 # call the simulation
                 ret = f(*args)
                 # push args into dbase
-                c.execute("INSERT INTO {0} VALUES ({1})".format(table, ", ".join([ str(a) for a in args]) ) )
+                c.execute("INSERT INTO {0} VALUES (?,?,?)".format(table),
+                           [args[0],args[1],ret] )
                 # commit
                 self.conn.commit()
                 # behave like the original
--- testA.py
+++ testA.py
@@ -2,11 +2,11 @@ from mydb import *
 
 
 sdb = SimDataDB("testA.db")
-sdb.Add_Table("results1",( ("x","FLOAT"), ("y","FLOAT") ))
+sdb.Add_Table("results1",
+              ( ("x","FLOAT"), ("y","FLOAT") ),
+              ( ("z","FLOAT"), )
+              )
 
-# mydb.setupDB("testA.db", "results1",( ("x","FLOAT"), ("y","FLOAT") ))
-
-# @mydb.scripter("testA.db", "results1")
 @sdb.Decorate("results1")
 def fake_sim(x,y):
     return x+2.0*y
@@ -15,7 +15,5 @@ fake_sim(1.0,1.5)
 fake_sim(1.0,1.5)
 fake_sim(1.0,2.5)
 
-
 rubs = sdb.Grab_All("results1")
 print rubs
-# runs = sdb.grab_all("testA.db", "results1")
