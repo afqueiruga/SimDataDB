bea53660837963b1a9266de20eff68717616460d
Alejandro Francisco Queiruga <afq@berkeley.edu>
2016-07-20 15:41:45 -0700

    Checks and returns fetched results

--- mydb.py
+++ mydb.py
@@ -51,10 +51,11 @@ class SimDataDB():
                 # check if arguments exist already
                 argcheck = " AND ".join([ "{0}={1}".format(argname[0],val)
                              for argname,val in zip(self.callsigs[table], args) ])
-                c.execute("SELECT EXISTS(SELECT 1 FROM {0} WHERE {1} LIMIT 1)".format(table, argcheck) )
-                
-                if c.fetchall()[0][0]==1:
-                    return None # TODO: act like a memoizer
+                c.execute("SELECT {2} FROM {0} WHERE {1} LIMIT 1".format(
+                    table, argcheck, " ".join([k[0] for k in self.retsigs[table]])) )
+                result = c.fetchone()
+                if result!=None:
+                    return result[0]
                 # call the simulation
                 ret = f(*args)
                 # push args into dbase
@@ -76,28 +77,3 @@ class SimDataDB():
     def __del__(self):
         self.conn.commit()
         self.conn.close()
-
-
-        
-def scripter(dbase, table):
-    def wrap(f):
-        def wrapper(*args):
-            # check if args already in dbase
-            conn = sqlite3.connect(dbase)
-            c = conn.cursor()
-            # c.execute("SELECT EXISTS(SELECT 1 FROM {0} WHERE {1} LIMIT 1)".fromat(table),)
-            # call the simulation
-            ret = f(*args)
-            # push args into dbase
-            
-            c.execute("INSERT INTO {0} VALUES ({1})".format(table, ", ".join([ str(a) for a in args]) ) )
-            conn.commit()
-            conn.close()
-        
-            # save results
-        
-            # behave like the original
-            return ret
-        return wrapper
-    return wrap
-
--- testB.py
+++ testB.py
@@ -11,9 +11,9 @@ sdb.Add_Table("results1",
 def fake_sim(x,y):
     return np.array([x,2*y])
 
-fake_sim(1.0,1.5)
-fake_sim(1.0,1.5)
-fake_sim(1.0,2.5)
+print fake_sim(1.0,1.5)
+print fake_sim(1.0,1.5)
+print fake_sim(1.0,2.5)
 
 rubs = sdb.Grab_All("results1")
 print rubs
