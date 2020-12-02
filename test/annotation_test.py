eefc2f5893762502f09cb3af4f0e999113942521
Alejandro Francisco Queiruga <afq@google.com>
2020-12-01 21:31:52 -0800

    extra tests for annotation version

--- test/annotation_test.py
+++ test/annotation_test.py
@@ -1,5 +1,6 @@
 from typing import Tuple
 
+import os
 import unittest as ut
 import tempfile, shutil
 
@@ -15,37 +16,62 @@ class annotation_test(ut.TestCase):
         shutil.rmtree(self.tmpdir)
 
     def test(self):
-        db_path = self.tmpdir + "/testA.db"
+        db_path = os.path.join(self.tmpdir, "testA.db")
 
         @SimDataDB2(db_path, "results1")
         def fake_sim(x: float, y: float) -> Tuple[float]:
             return x + 2.0 * y,
 
-        fake_sim(1.0, 1.5)
-        fake_sim(1.0, 1.5)
-        fake_sim(1.0, 2.5)
+        self.assertAlmostEqual(fake_sim(1.0, 1.5)[0], 4.0)
+        # This one should be memoized:
+        self.assertAlmostEqual(fake_sim(1.0, 1.5)[0], 4.0)
+        self.assertAlmostEqual(fake_sim(1.0, 2.5)[0], 6.0)
 
         sdb = SimDataDB2(db_path, "results1")
         results = sdb.GrabAll()
         print(results)
-        self.assertTrue(True)
+        self.assertEqual(len(results), 2)
 
     def test_new_dir(self):
-        db_path = self.tmpdir + "/non/existent/path/testA.db"
+        db_path = os.path.join(self.tmpdir, "non/existent/path/testA.db")
 
         @SimDataDB2(db_path, "results1")
         def fake_sim(x: float, y: float) -> Tuple[float]:
             return x + 2.0 * y,
 
-        fake_sim(1.0, 1.5)
-        fake_sim(1.0, 1.5)
-        fake_sim(1.0, 2.5)
+        self.assertAlmostEqual(fake_sim(1.0, 1.5)[0], 4.0)
+        # This one should be memoized:
+        self.assertAlmostEqual(fake_sim(1.0, 1.5)[0], 4.0)
+        self.assertAlmostEqual(fake_sim(1.0, 2.5)[0], 6.0)
 
         sdb = SimDataDB2(db_path, "results1")
         results = sdb.GrabAll()
         print(results)
-        self.assertTrue(True)
+        self.assertEqual(len(results), 2)
 
+    def test_more_types(self):
+        db_path = os.path.join(self.tmpdir, "testA.db")
+
+        @SimDataDB2(db_path, "results1")
+        def fake_sim(x: int, y: str) -> Tuple[int, str]:
+            return x + 1, y + "foo"
+
+        a, b = fake_sim(1, "foo")
+        self.assertEqual(a, 2)
+        self.assertEqual(b, "foofoo")
+        # This one should be memoized:
+        a, b = fake_sim(1, "foo")
+        self.assertEqual(a, 2)
+        self.assertEqual(b, "foofoo")
+        a, b = fake_sim(2, "yap")
+        self.assertEqual(a, 3)
+        self.assertEqual(b, "yapfoo")
+        
+        sdb = SimDataDB2(db_path, "results1")
+        results = sdb.GrabAll()
+        print(results)
+        # Only two unique runs:
+        self.assertEqual(len(results), 2)
 
 if __name__ == "__main__":
     ut.main()
